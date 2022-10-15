51e48a04dd0e8101d95726631f4219cc40a1e428
zkp <442910864@qq.com>
2022-10-15 15:41:25 +0800

    test✅:

--- src/tsc.ts
+++ src/tsc.ts
@@ -1,14 +1,14 @@
 
-// export * from './componentFns';
-// export * from './components';
-// export * from './utils';
-
-export {default as Exhibit} from './components/Exhibit';
-export {default as MyQueryForm} from './components/MyQueryForm';
-export {default as MyTable, renderSelectionCellFn} from './components/MyTable';
-export {default as MyModal, createModalFn} from './components/MyModal';
-export {default as SmallParticle} from './components/SmallParticle';
-export * from './components/MyQueryForm/modules';
-
 export * from './componentFns';
+export * from './components';
 export * from './utils';
+
+// export {default as Exhibit} from './components/Exhibit';
+// export {default as MyQueryForm} from './components/MyQueryForm';
+// export {default as MyTable, renderSelectionCellFn} from './components/MyTable';
+// export {default as MyModal, createModalFn} from './components/MyModal';
+// export {default as SmallParticle} from './components/SmallParticle';
+// export * from './components/MyQueryForm/modules';
+
+// export * from './componentFns';
+// export * from './utils';
