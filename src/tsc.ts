62cebdc24e4ec5893025c870b577b93f930e6cb6
zkp <442910864@qq.com>
2022-10-15 12:26:01 +0800

    test✅:

--- src/tsc.ts
+++ src/tsc.ts
@@ -4,3 +4,8 @@
 // export * from './utils';
 
 export {default as Test2} from './Test2';
+export {default as Exhibit} from './components/Exhibit';
+export {default as MyQueryForm} from './components/MyQueryForm';
+export {default as MyTable, renderSelectionCellFn} from './components/MyTable';
+export {default as MyModal, createModalFn} from './components/MyModal';
+export {default as SmallParticle} from './components/SmallParticle';
