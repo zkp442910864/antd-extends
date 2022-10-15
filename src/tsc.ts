b9d06a2bf83511fb06c1f7ad9efb41478e9dcdc3
zkp <442910864@qq.com>
2022-10-15 12:20:57 +0800

    test✅:

--- src/tsc.ts
+++ src/tsc.ts
@@ -1,6 +1,6 @@
 
-export * from './componentFns';
-export * from './components';
-export * from './utils';
+// export * from './componentFns';
+// export * from './components';
+// export * from './utils';
 
 export {default as Test2} from './Test2';
